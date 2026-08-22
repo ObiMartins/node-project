2227adc18a7c773d03b237921a9ccf434648bda9
Obi Martins <73021405+ObiMartins@users.noreply.github.com>
2026-08-22 09:23:33 +0000

    Modify application message to App is running

--- src/server.js
+++ src/server.js
@@ -11,7 +11,7 @@ app.use(express.json());
 
 app.get('/', (req, res) => {
   res.json({
-    message: 'Simple Node.js API is running staging',
+    message: 'Simple Node.js API is running',
     status: 'success',
     runningWith: 'PM2 ready'
   });
